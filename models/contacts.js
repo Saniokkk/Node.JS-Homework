a41bb9d9b5fb44dd30e023268b6777c4e582a615
Aleksandr.Kashirin <saniok26@rambler.ru>
2022-07-19 19:01:52 +0300

    models/contacts fix

--- models/contacts.js
+++ models/contacts.js
@@ -1,6 +1,7 @@
 const fs = require("fs/promises");
 const path = require("path");
 const { v4: uuid } = require('uuid');
+
 const contactsPath = path.join(__dirname, "contacts.json");
 
 /* update list contacts */
